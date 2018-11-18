aaaaa42af1c3e55fc2a1dde3489d0e77d0a12d99
matyi92 <vasmatyas92@gmail.com>
2018-11-18 19:06:36 +0200

    Feature/change resource status (#7)
    * Loading test data to embedded mongo repository.
    
    * Get ResourceLogs controller; Extracted ResourceLogs outside of Resource; Replaced Date with Instant.
    
    * Merged resourcelog branch.
    
    * Status update implementation

---
+++ src/main/java/com/resource/management/controller/UpdateResourceStatusController.java
@@ -0,0 +1,67 @@
+/************************************************************************
+ ** PROJECT:   XVP
+ ** LANGUAGE:  Java, J2SE JDK 1.8
+ **
+ ** COPYRIGHT: FREQUENTIS AG
+ **            Innovationsstrasse 1
+ **            A-1100 VIENNA
+ **            AUSTRIA
+ **            tel +43 1 811 50-0
+ **
+ ** The copyright to the computer program(s) herein
+ ** is the property of Frequentis AG, Austria.
+ ** The program(s) shall not be used and/or copied without
+ ** the written permission of Frequentis AG.
+ **
+ ************************************************************************/
+package com.resource.management.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.resource.management.api.SubUnitUpdatedNotification;
+import com.resource.management.api.UpdateResourceStatusRequest;
+import com.resource.management.data.Resource;
+import com.resource.management.data.SubUnit;
+import com.resource.management.data.SubUnitsRepository;
+
+@Controller
+public class UpdateResourceStatusController
+{
+   @Autowired
+   private SubUnitsRepository repository;
+
+
+   @MessageMapping("/updatestatus")
+   @SendTo("/topic/subunits")
+   public SubUnitUpdatedNotification handle( final UpdateResourceStatusRequest request )
+   {
+      SubUnitUpdatedNotification notification = null;
+      List<SubUnit> subUnits = repository.findAll();
+      final Optional<Resource> resourceOptional =
+            subUnits.stream()
+                  .flatMap( subUnit -> subUnit.getResources().stream()
+                        .filter( resource -> resource.getPlateNumber().equals( request.getPlateNumber() ) ) )
+                  .findFirst();
+
+      if ( resourceOptional.isPresent() )
+      {
+         resourceOptional.get().setStatus( request.getResourceStatus() );
+         repository.saveAll( subUnits );
+
+         final Optional<SubUnit> subUnit =
+               subUnits.stream()
+                     .filter( s -> s.getResources().stream()
+                           .anyMatch( resource -> resource.getPlateNumber().equals( request.getPlateNumber() ) ) )
+                     .findFirst();
+         notification = new SubUnitUpdatedNotification( subUnit.get() );
+      }
+
+      return notification;
+   }
+}
