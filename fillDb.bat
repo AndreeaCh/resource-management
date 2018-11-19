5f904f339c5528410e39033535e5e84b5d59335a
CHISCARI Andreea <Andreea.CHISCARI@frequentis.com>
2018-11-19 18:11:40 +0200

    Added no args constructor

--- fillDb.bat
+++ fillDb.bat
@@ -7,7 +7,7 @@ set MONGOADDR=%~1%\mongo.exe
 set MONGOIMPORTADDR=%~1%\mongoimport.exe
 
 "%MONGOADDR%" --eval db.dropDatabase(^)
-for /l %%x in (1, 1, 5) do (
+for /l %%x in (1, 1, 10) do (
    echo Adding element number: %%x
    "%MONGOIMPORTADDR%" -d test -c subUnit --file "%~dp0\src\main\resources\subUnit%%x.json"
    )
