ad45f9ad7a3ba203e40d43eb264ed80e347d1a09
CHISCARI Andreea <Andreea.CHISCARI@frequentis.com>
2018-11-20 17:55:06 +0200

    Added last updated timestamp per subunit.

---
+++ src/main/doc/add.md
@@ -0,0 +1,78 @@
+# Editing a Sub-Unit
+
+A client can start editing a sub-unit by sending the following message:
+
+``` javascript
+MESSAGE
+id:lock-req-1
+destination:/topic/addSubUnit
+[
+  {
+    "name": "CJ"
+  }
+]
+```
+
+The response can be a success or a failure :
+
+``` javascript
+MESSAGE
+id:lock-req-1
+destination:/topic/subunits
+[
+  {
+    "statusCode": "OK"
+  }
+]
+```
+
+``` javascript
+MESSAGE
+id:lock-req-1
+destination:/topic/subunits
+[
+  {
+    "statusCode": "ERROR"
+  }
+]
+```
+
+When a sub-unit is added all the clients will receive the following message:
+
+``` javascript
+MESSAGE
+subscription:sub-1
+message-id:sub-notification-123
+destination:/topic/subunits
+content-type:text/json
+
+[
+  {
+    "name": "CJ",
+    "resources": [
+      {
+        "identificationNumber": "1234",
+        "plateNumber": "CJ04ASD",
+        "vehicleType": "APCA",
+        "captain": "Pt. Major John Doe",
+        "crew": [
+          "Lt. Anne Doe",
+          "Ben Doe"
+        ],
+        "status": "AVAILABLE_IN_GARAGE"
+      },
+      {
+        "identificationNumber": "1235",
+        "plateNumber": "CJ05ASD",
+        "vehicleType": "EPA",
+        "captain": "Pt. Major Jack Moe",
+        "crew": [
+          "Lt. Anne Moe",
+          "Ben Moe"
+        ],
+        "status": "IN_INTERVENTION"
+      }
+    ]
+  }
+]
+```
--- src/test/java/com/resource/management/controller/AddSubUnitControllerTest.java
+++ src/test/java/com/resource/management/controller/AddSubUnitControllerTest.java
@@ -2,24 +2,32 @@ package com.resource.management.controller;
 
 import com.resource.management.SubUnitsTestDataUtils;
 import com.resource.management.api.AddSubUnitRequest;
+import com.resource.management.api.AddSubUnitResponse;
 import com.resource.management.api.SubUnitUpdatedNotification;
 import com.resource.management.data.SubUnitsRepository;
+import com.resource.management.service.NotificationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static com.resource.management.api.StatusCode.OK;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AddSubUnitControllerTest {
     @MockBean
     private SubUnitsRepository subUnitsRepository;
+
+    @MockBean
+    private NotificationService notificationService;
 
     @Autowired
     private AddSubUnitController sut;
@@ -30,15 +38,34 @@ public class AddSubUnitControllerTest {
     }
 
     @Test
+    public void handleAddSubUnitRequest_sut_respondsWithSuccess() {
+        //given
+        AddSubUnitRequest request = new AddSubUnitRequest(SubUnitsTestDataUtils.loadRandomSubUnit());
+
+        //when
+        AddSubUnitResponse response = this.sut.handle(request);
+
+        //then
+        assertThat(
+                "Expected status code to be OK.",
+                response.getStatusCode(),
+                equalTo(OK));
+    }
+
+    @Test
     public void handleAddSubUnitRequest_sut_publishesNewSubUnit() {
         //given
         AddSubUnitRequest request = new AddSubUnitRequest(SubUnitsTestDataUtils.loadRandomSubUnit());
 
         //when
-        SubUnitUpdatedNotification notification = this.sut.handle(request);
+        this.sut.handle(request);
 
         //then
-        assertThat("Expected notification to contain the added sub-unit.", notification.getSubUnit(),
+        ArgumentCaptor<SubUnitUpdatedNotification> captor = ArgumentCaptor.forClass(SubUnitUpdatedNotification.class);
+        verify(notificationService).publishSubUnitNotification(captor.capture());
+        assertThat(
+                "Expected that the notification contains the deleted subunit name",
+                captor.getValue().getSubUnit(),
                 equalTo(request.getSubUnit()));
     }
 }
