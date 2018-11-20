deebb814bb8da907ab8d53541b14c305ad76e3e8
AndreeaCh <andreeachiscari@gmail.com>
2018-11-20 21:58:26 +0200

    New changelist

--- fillDb.bat
+++ fillDb.bat
@@ -1,11 +1,14 @@
 @echo off
+if "%~1%"=="" (
+   echo Please specify the path to the /bin folder of your mongoDB installation
+) else (
 
-set MONGOADDR=C:\Dev\mongo\bin\mongo.exe
-set MONGOIMPORTADDR=C:\Dev\mongo\bin\mongoimport.exe
+set MONGOADDR=%~1%\mongo.exe
+set MONGOIMPORTADDR=%~1%\mongoimport.exe
 
 "%MONGOADDR%" --eval db.dropDatabase(^)
 for /l %%x in (1, 1, 10) do (
    echo Adding element number: %%x
    "%MONGOIMPORTADDR%" -d test -c subUnit --file "%~dp0\src\main\resources\subUnit%%x.json"
    )
-
+)
