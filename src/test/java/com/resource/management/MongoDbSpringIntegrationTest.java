64edd3fb0af7471c742578a8777d308e815fc346
ATruta <Andrei.TRUTA@frequentis.com>
2018-12-12 18:31:36 +0200

    Fix build issues

--- src/test/java/com/resource/management/MongoDbSpringIntegrationTest.java
+++ src/test/java/com/resource/management/MongoDbSpringIntegrationTest.java
@@ -2,6 +2,8 @@ package com.resource.management;
 
 import com.resource.management.resource.model.SubUnit;
 import com.resource.management.resource.model.SubUnitsRepository;
+import java.time.Instant;
+import java.util.Collections;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -9,9 +11,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.time.Instant;
-import java.util.Collections;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,7 +22,7 @@ public class MongoDbSpringIntegrationTest {
 
     @Test
     public void test() {
-        final SubUnit subUnit = new SubUnit("CJ", Collections.emptyList(), Instant.now().toString(), null, true);
+        final SubUnit subUnit = new SubUnit("CJ", Collections.emptyList(), Collections.emptyList(), Instant.now().toString(), null, true);
         this.repository.save(subUnit);
 
         Assert.assertEquals("CJ", this.repository.findAll().get(0).getName());
