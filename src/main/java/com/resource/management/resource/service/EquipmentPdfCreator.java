a5dc1bcdcf1825f0c230d47c9fa2f14fec9f6fa0
AndreeaCh <andreeachiscari@gmail.com>
2019-01-14 21:32:30 +0200

    WIP Report

--- src/main/java/com/resource/management/resource/service/EquipmentPdfCreator.java
+++ src/main/java/com/resource/management/resource/service/EquipmentPdfCreator.java
@@ -30,6 +30,22 @@ public class EquipmentPdfCreator {
     public static final String EQUIPMENT_REPORT_FILE_NAME = "RaportEchipamente.pdf";
     private static final String ARIAL = "fonts/arial.ttf";
     private static final String ARIAL_BOLD = "fonts/arialbd.ttf";
+
+    private static final String TITLE = "TEHNICA DE INTERVENȚIE OPERAȚIONALĂ ȘI NEOPERAȚIONALĂ A I.S.U. CLUJ ÎN DATA DE ";
+    private static final String TOTAL = "TOTAL";
+    private static final String NEOPERATIONAL = "Neoperațional";
+    private static final String REZERVA = "Rezervă";
+    private static final String OPERATIONAL = "Operațional";
+    private static final String GARDA_DE_INTERVENTIE = "Garda de intervenție";
+    private static final String TEHNICA_DE_INTERVENTIE = "Tehnică de intervenție";
+    private static final String TOTAL_TEHNICA_DE_INTERVENTIE = "TOTAL Tehnică de intervenție";
+    private static final String ECHIPAMENTE = "Echipamente";
+    private static final String TOTAL_ECHIPAMENTE = "TOTAL Echipamente";
+    private static final String TEHNICA_DE_PRIMA_INTERVENTIE = "Tehnică de primă intervenție";
+    private static final String TOTAL_TEHNICA_DE_PRIMA_INTERVENTIE = "TOTAL Tehnică de primă intervenție";
+    private static final String ALTE_TIPURI_DE_TEHNICA = "Alte tipuri de tehnică";
+    private static final String TOTAL_ALTE_TIPURI_DE_TEHNICA = "TOTAL Alte tipuri de tehnică";
+
     private BaseFont bf = BaseFont.createFont(ARIAL, BaseFont.IDENTITY_H, true);
     private BaseFont bfb = BaseFont.createFont(ARIAL_BOLD, BaseFont.IDENTITY_H, true);
     private Font cellFont = new Font(bf, 10);
@@ -55,7 +71,7 @@ public class EquipmentPdfCreator {
             table.setWidthPercentage(90);
             table.setSpacingBefore(0);
             table.setSpacingAfter(0);
-            addTableHeader(table, report);
+            createTableHeader(table, report);
             addDetailsToTable(table, report);
             document.add(table);
             document.close();
@@ -67,7 +83,7 @@ public class EquipmentPdfCreator {
     private void addTitle(Document document) throws DocumentException {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         LocalDateTime now = LocalDateTime.now();
-        Paragraph paragraph = new Paragraph("TEHNICA DE INTERVENȚIE OPERAȚIONALĂ ȘI NEOPERAȚIONALĂ A I.S.U. CLUJ ÎN DATA DE " + dtf.format(now), cellFontTitle);
+        Paragraph paragraph = new Paragraph(TITLE + dtf.format(now), cellFontTitle);
         paragraph.setAlignment(Element.ALIGN_CENTER);
         document.add(paragraph);
         document.add(Chunk.NEWLINE);
@@ -83,14 +99,14 @@ public class EquipmentPdfCreator {
                 });
 
         SubUnitReport raportAllSubUnits = report.getTotals();
-        addHeaderCell(table, "TOTAL", 0, 1, 3);
+        addHeaderCell(table, TOTAL, 0, 1, 3);
         addUsableResources(table, raportAllSubUnits);
         addReserveResources(table, raportAllSubUnits);
         addUnusableResources(table, raportAllSubUnits);
     }
 
     private void addUnusableResources(PdfPTable table, SubUnitReport subUnitReport) {
-        addHeaderCell(table, "Neoperațional", 0, 1, 1);
+        addHeaderCell(table, NEOPERATIONAL, 0, 1, 1);
         subUnitReport.getFirstInterventionResourceReport()
                 .forEach((type, r) -> {
                     addSimpleTableCell(table, r.getUnusableAsString());
@@ -110,7 +126,7 @@ public class EquipmentPdfCreator {
     }
 
     private void addReserveResources(PdfPTable table, SubUnitReport subUnitReport) {
-        addHeaderCell(table, "Rezervă", 0, 1, 1);
+        addHeaderCell(table, REZERVA, 0, 1, 1);
         subUnitReport.getFirstInterventionResourceReport()
                 .forEach((type, r) -> {
                     addSimpleTableCell(table, r.getReserveAsString());
@@ -130,7 +146,7 @@ public class EquipmentPdfCreator {
     }
 
     private void addUsableResources(PdfPTable table, SubUnitReport subUnitReport) {
-        addHeaderCell(table, "Operațional", 0, 1, 1);
+        addHeaderCell(table, OPERATIONAL, 0, 1, 1);
         subUnitReport.getFirstInterventionResourceReport()
                 .forEach((type, r) -> {
                     addSimpleTableCell(table, r.getUsableAsString());
@@ -166,22 +182,21 @@ public class EquipmentPdfCreator {
         }
     }
 
-    private void addTableHeader(PdfPTable table, AllSubUnitsReport report) {
+    private void createTableHeader(PdfPTable table, AllSubUnitsReport report) {
         SubUnitReport subUnitReport = report.getSubUnitReports().values().stream().findFirst().orElse(null);
         int firstInterventionResources = subUnitReport.getFirstInterventionResourceReport().size();
         int otherResourcesSize = subUnitReport.getOtherResourceReport().size();
         int equipmentsSize = subUnitReport.getEquipmentReport().size();
 
-        addHeaderCell(table, "Garda de intervenție", 90, 2, 3);
-        addHeaderCell(table, "Tehnică de intervenție", 0, firstInterventionResources + otherResourcesSize + 2, 1);
-        addHeaderCell(table, "TOTAL Tehnică de intervenție", 90, 1, 3);
-        addHeaderCell(table, "Echipamente", 0, equipmentsSize, 2);
-        addHeaderCell(table, "TOTAL Echipamente", 90, 1, 3);
-        //table.completeRow();
-        addHeaderCell(table, "Tehnică de primă intervenție", 0, firstInterventionResources, 1);
-        addHeaderCell(table, "TOTAL Tehnică de primă intervenție", 90, 1, 2);
-        addHeaderCell(table, "Alte tipuri de tehnică", 0, otherResourcesSize, 1);
-        addHeaderCell(table, "TOTAL Alte tipuri de tehnică", 90, 1, 2);
+        addHeaderCell(table, GARDA_DE_INTERVENTIE, 90, 2, 3);
+        addHeaderCell(table, TEHNICA_DE_INTERVENTIE, 0, firstInterventionResources + otherResourcesSize + 2, 1);
+        addHeaderCell(table, TOTAL_TEHNICA_DE_INTERVENTIE, 90, 1, 3);
+        addHeaderCell(table, ECHIPAMENTE, 0, equipmentsSize, 2);
+        addHeaderCell(table, TOTAL_ECHIPAMENTE, 90, 1, 3);
+        addHeaderCell(table, TEHNICA_DE_PRIMA_INTERVENTIE, 0, firstInterventionResources, 1);
+        addHeaderCell(table, TOTAL_TEHNICA_DE_PRIMA_INTERVENTIE, 90, 1, 2);
+        addHeaderCell(table, ALTE_TIPURI_DE_TEHNICA, 0, otherResourcesSize, 1);
+        addHeaderCell(table, TOTAL_ALTE_TIPURI_DE_TEHNICA, 90, 1, 2);
 
         subUnitReport.getFirstInterventionResourceReport().keySet().forEach(r -> {
             addHeaderCell(table, r, 90, 1, 1);
