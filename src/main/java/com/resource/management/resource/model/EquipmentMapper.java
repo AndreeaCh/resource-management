a725561d72b30c316e6262b66076a0e051dd900f
Adrian Galos <adrian.galos@gmail.com>
2019-01-28 20:33:37 +0200

    enhanceEquipment: Change to equipment id

--- src/main/java/com/resource/management/resource/model/EquipmentMapper.java
+++ src/main/java/com/resource/management/resource/model/EquipmentMapper.java
@@ -4,7 +4,7 @@ public final class EquipmentMapper {
     public static final Equipment toInternal(final com.resource.management.api.resources.Equipment externalEquipment) {
         Equipment equipment = new Equipment();
 
-        equipment.setEquipmentId(externalEquipment.getEquipmentId());
+        equipment.setEquipmentId(externalEquipment.getId());
         equipment.setEquipmentType(externalEquipment.getEquipmentType());
         equipment.setReserves(externalEquipment.getReserves());
         equipment.setUsable(externalEquipment.getUsable());
@@ -15,7 +15,7 @@ public final class EquipmentMapper {
 
     public static final com.resource.management.api.resources.Equipment toApi(final Equipment internalEquipment) {
         com.resource.management.api.resources.Equipment equipment = new com.resource.management.api.resources.Equipment();
-        equipment.setEquipmentId(internalEquipment.getEquipmentId());
+        equipment.setId(internalEquipment.getEquipmentId());
         equipment.setEquipmentType(internalEquipment.getEquipmentType());
         equipment.setReserves(internalEquipment.getReserves());
         equipment.setUsable(internalEquipment.getUsable());
