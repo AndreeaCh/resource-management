aa85298a80ebb318d032902894e38eb70708b7cd
Marius Barta <marius.barta@gmail.com>
2019-02-03 02:05:11 +0200

    Hardcoded paths can now be supplied as script arguments

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,4 @@
-java -jar target/resource-management-0.0.1-SNAPSHOT.jar
+SET _INSTALL_PATH=%1
+IF "%_INSTALL_PATH%"=="" SET _INSTALL_PATH=target
+
+java -jar %_INSTALL_PATH%/resource-management-0.0.1-SNAPSHOT.jar
--- fillDb.bat
+++ fillDb.bat
@@ -3,18 +3,21 @@ if "%~1%"=="" (
    echo Please specify the path to the /bin folder of your mongoDB installation
 ) else (
 
+SET _IMPORT_PATH=%2
+IF "%_IMPORT_PATH%"=="" SET _IMPORT_PATH=%~dp0\src\main\resources
+
 set MONGOADDR=%~1%\mongo.exe
 set MONGOIMPORTADDR=%~1%\mongoimport.exe
 
 "%MONGOADDR%" --eval db.dropDatabase(^)
 for /l %%x in (0, 1, 10) do (
    echo Adding element number: %%x
-   "%MONGOIMPORTADDR%" -d test -c subUnit --file "%~dp0\src\main\resources\subUnit%%x.json"
+   "%MONGOIMPORTADDR%" -d test -c subUnit --file "%_IMPORT_PATH%\subUnit%%x.json"
    )
 )
 
 for /l %%x in (0, 1, 2) do (
    echo Adding service number: %%x
-   "%MONGOIMPORTADDR%" -d test -c service --file "%~dp0\src\main\resources\service%%x.json"
+   "%MONGOIMPORTADDR%" -d test -c service --file "%_IMPORT_PATH%\service%%x.json"
    )
 )
