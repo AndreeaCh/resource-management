7b06a15605f908057c39f64ba3f2c1ea049c4df3
Marius Barta <marius.barta@gmail.com>
2019-02-03 02:05:11 +0200

    Hardcoded paths can now be supplied as script arguments

--- fillDb.bat
+++ fillDb.bat
@@ -3,30 +3,33 @@ if "%~1%"=="" (
    echo Please specify the path to the /bin folder of your mongoDB installation
 ) else (
 
+SET _IMPORT_PATH=%2
+IF "%_IMPORT_PATH%"=="" SET _IMPORT_PATH=%~dp0\src\main\resources
+
 set MONGOADDR=%~1%\mongo.exe
 set MONGOIMPORTADDR=%~1%\mongoimport.exe
 
 "%MONGOADDR%" --eval db.dropDatabase(^)
 for /l %%x in (0, 1, 10) do (
    echo Adding subunit number: %%x
-   "%MONGOIMPORTADDR%" -d test -c subUnit --file "%~dp0\src\main\resources\subUnit%%x.json"
+   "%MONGOIMPORTADDR%" -d test -c subUnit --file "%_IMPORT_PATH%\subUnit%%x.json"
    )
 )
 
 for /l %%x in (0, 1, 2) do (
    echo Adding service number: %%x
-   "%MONGOIMPORTADDR%" -d test -c service --file "%~dp0\src\main\resources\service%%x.json"
+   "%MONGOIMPORTADDR%" -d test -c service --file "%_IMPORT_PATH%\service%%x.json"
    )
 )
 
 for /l %%x in (0, 1, 0) do (
    echo Adding function number: %%x
-   "%MONGOIMPORTADDR%" -d test -c functions --file "%~dp0\src\main\resources\function%%x.json"
+   "%MONGOIMPORTADDR%" -d test -c functions --file "%_IMPORT_PATH%\function%%x.json"
    )
 )
 
 for /l %%x in (0, 1, 0) do (
    echo Adding vehicle number: %%x
-   "%MONGOIMPORTADDR%" -d test -c vehicleTypes --file "%~dp0\src\main\resources\vehicle%%x.json"
+   "%MONGOIMPORTADDR%" -d test -c vehicleTypes --file "%_IMPORT_PATH%\vehicle%%x.json"
    )
 )
