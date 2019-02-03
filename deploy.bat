e4d5f157bbd3785ea8ff4c03bba10b375d75d2be
Marius Barta <marius.barta@gmail.com>
2019-02-03 02:05:11 +0200

    Hardcoded paths can now be supplied as script arguments

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,7 @@
-java -Dspring.config.location=D:\\logs\\application.properties -jar target/resource-management-0.0.1-SNAPSHOT.jar
+SET _INSTALL_PATH=%1
+IF "%_INSTALL_PATH%"=="" SET _INSTALL_PATH=target
+
+SET _CONFIG_PATH=%2
+IF "%_CONFIG_PATH%"=="" SET _CONFIG_PATH=D:\\logs\\application.properties
+
+java -Dspring.config.location=%_CONFIG_PATH% -jar %_INSTALL_PATH%/resource-management-0.0.1-SNAPSHOT.jar
