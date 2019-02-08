111c59c77bd8d8bbe08c7a153e41e67b4d20026e
Marius Barta <marius.barta@gmail.com>
2019-02-08 15:15:33 +0200

    Fix import glitch

--- fillDb.bat
+++ fillDb.bat
@@ -1,7 +1,9 @@
-@echo off
+ECHO OFF
+
 if "%~1%"=="" (
-   echo Please specify the path to the /bin folder of your mongoDB installation
-) else (
+   ECHO Please specify the path to the /bin folder of your mongoDB installation
+   GOTO :cleaning
+)
 
 SET _IMPORT_PATH=%2
 IF "%_IMPORT_PATH%"=="" SET _IMPORT_PATH=%~dp0\src\main\resources
@@ -11,25 +13,28 @@ set MONGOIMPORTADDR=%~1%\mongoimport.exe
 
 "%MONGOADDR%" --eval db.dropDatabase(^)
 for /l %%x in (0, 1, 10) do (
-   echo Adding subunit number: %%x
+   ECHO Adding subunit number: %%x
    "%MONGOIMPORTADDR%" -d test -c subUnit --file "%_IMPORT_PATH%\subUnit%%x.json"
    )
 )
 
 for /l %%x in (0, 1, 2) do (
-   echo Adding service number: %%x
+   ECHO Adding service number: %%x
    "%MONGOIMPORTADDR%" -d test -c service --file "%_IMPORT_PATH%\service%%x.json"
    )
 )
 
 for /l %%x in (0, 1, 0) do (
-   echo Adding function number: %%x
+   ECHO Adding function number: %%x
    "%MONGOIMPORTADDR%" -d test -c functions --file "%_IMPORT_PATH%\function%%x.json"
    )
 )
 
 for /l %%x in (0, 1, 0) do (
-   echo Adding vehicle number: %%x
+   ECHO Adding vehicle number: %%x
    "%MONGOIMPORTADDR%" -d test -c vehicleTypes --file "%_IMPORT_PATH%\vehicle%%x.json"
    )
 )
+
+:cleaning
+ECHO ON
