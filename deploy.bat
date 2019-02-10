7e03dc9f852638dddb8bbf4773927fc70cc9afe8
Marius Barta <marius.barta@gmail.com>
2019-02-10 23:17:26 +0200

    Added cli script to run from anywhere, using environment paths

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
-SET _INSTALL_PATH=%1
-IF "%_INSTALL_PATH%"=="" SET _INSTALL_PATH=target
+SET _JAR_PATH=%1
+IF "%_JAR_PATH%"=="" SET _JAR_PATH=target/resource-management-0.0.1-SNAPSHOT.jar
 
 SET _CONFIG_PATH=%2
 IF "%_CONFIG_PATH%"=="" SET _CONFIG_PATH=D:\\logs\\application.properties
 
-java -Dspring.config.location=%_CONFIG_PATH% -jar %_INSTALL_PATH%/resource-management-0.0.1-SNAPSHOT.jar
+java -Dspring.config.location=%_CONFIG_PATH% -jar %_JAR_PATH%
