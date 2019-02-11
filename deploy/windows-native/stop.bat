ae738194d8a7dccd04d8a3363c9dfc0c6d9e4390
Marius Barta <marius.barta@gmail.com>
2019-02-11 22:26:54 +0200

    Dependencies are to be uninstalled separately

--- deploy/windows-native/stop.bat
+++ deploy/windows-native/stop.bat
@@ -4,16 +4,11 @@
 
 ECHO OFF
 
-:: TODO: processes neeed to be identified precisely, otherwise we end up stopping other processes
-
 ECHO STOP_1 Stopping frontend server ...
 TASKKILL /IM node.exe /F
 
 ECHO STOP_2 Stopping backend server ...
 TASKKILL /IM java.exe /F
-
-ECHO STOP_2 Stopping backend server ...
-TASKKILL /IM mongod.exe /F
 
 :cleaning
 ECHO ON
