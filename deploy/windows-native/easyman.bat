f05a97cabb81791c05085c51b91a1fa40837645d
Marius Barta <marius.barta@gmail.com>
2019-02-13 00:30:34 +0200

    Small refactoring

--- deploy/windows-native/easyman.bat
+++ deploy/windows-native/easyman.bat
@@ -1,31 +1,37 @@
 @echo off
 
-set COMMAND=
+set _COMMAND=
 if "%1%"=="start" (
-    cd %EASYMAN_HOME% & .\start.bat
+    SET _COMMAND=start.bat
 )
 if "%1%"=="stop" (
-   cd %EASYMAN_HOME% & .\stop.bat
+    SET _COMMAND=stop.bat
 )
 
 if "%1%"=="import" (
-   cd %EASYMAN_HOME% & .\import.bat %MONGO_HOME%\bin %EASYMAN_HOME%\import
+    SET _COMMAND=import.bat %MONGO_HOME%\bin %EASYMAN_HOME%\import
 )
 
 if "%1%"=="uninstall" (
-   cd %EASYMAN_HOME% & .\uninstall.bat
+    SET _COMMAND=uninstall.bat
 )
 
 if "%1%"=="uninstall-all" (
-   cd %EASYMAN_HOME% & .\uninstall.bat all
+    SET _COMMAND=uninstall.bat all
 )
 
-if "%1%"=="help" (
-   echo Usage: easyman "[start|stop|import|uninstall|help]"
-)
 
-if "%~1%"=="" (
-   echo Usage: easyman "[start|stop|import|uninstall|help]"
+if "%_COMMAND%"=="" (
+    echo Usage: easyman "[option]"
+    echo Options:
+    echo    start                 - start application( frontend, backend, db)
+    echo    stop                  - stops application( frontend, backend)
+    echo    import                - imports initial data into the db server
+    echo    uninstall             - uninstalls application
+    echo    uninstall-all         - uninstalls application and its dependencies
+    echo    help                  - show help
+) else  (
+    cd %EASYMAN_HOME% & .\%_COMMAND%
 )
 
 :: TODO : the help menu should be printed for any unrecognized option
