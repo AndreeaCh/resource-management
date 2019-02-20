e240777dbfd97c56ca99a21779da15f6036a206f
Marius Barta <marius.barta@gmail.com>
2019-02-20 15:02:46 +0000

    Maven profile for docker build

--- src/main/docker/backend/start-service.sh
+++ src/main/docker/backend/start-service.sh
@@ -2,8 +2,10 @@
 
 LOG_DIR=~/logs
 APP_DIR=~/bin
+CFG_DIR=~/config
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 
-java -jar $APP_DIR/easy-manage.jar
+java -Dspring.config.location=$CFG_DIR/application.properties -jar $APP_DIR/easy-manage.jar
+
 # TODO: redirect log > $LOG_DIR/backend-$DATE_WITH_TIME.log 2>&1
