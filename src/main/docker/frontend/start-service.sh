918ad7dd98d663ea8fb3f1985daf0f2e781ebaa7
Marius Barta <marius.barta@gmail.com>
2019-02-20 15:02:46 +0000

    Maven profile for docker build

--- src/main/docker/frontend/start-service.sh
+++ src/main/docker/frontend/start-service.sh
@@ -5,5 +5,8 @@ APP_DIR=~/bin
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 
+# modify backend server address for the frontend to connect to
+sed -i 's/localhost/${SERVER_ADDRESS}/g' $APP_DIR/dist/static/env.js
+
 http-server $APP_DIR/dist -p 8080
 # TODO: redirect log > $LOG_DIR/frontend-$DATE_WITH_TIME.log 2>&1
