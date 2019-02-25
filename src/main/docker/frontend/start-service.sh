69cdaeef72ab7a1c5c55fed3bda5ce3528ac351b
Marius Barta <marius.barta@gmail.com>
2019-02-25 23:07:22 +0200

    Paths and env var fixes

--- src/main/docker/frontend/start-service.sh
+++ src/main/docker/frontend/start-service.sh
@@ -6,7 +6,7 @@ APP_DIR=~/bin
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 
 # modify backend server address for the frontend to connect to
-sed -i 's/localhost/${SERVER_ADDRESS}/g' $APP_DIR/dist/static/env.js
+sed -i "s/localhost/${SERVER_ADDRESS:-localhost}/g" $APP_DIR/dist/static/env.js
 
 http-server $APP_DIR/dist -p 8080 > $LOG_DIR/frontend-$DATE_WITH_TIME.log 2>&1
 
