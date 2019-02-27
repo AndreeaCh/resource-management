1972e2711fc54cbddbbb316e9eed935561cb1f75
Marius Barta <marius.barta@gmail.com>
2019-02-27 23:33:49 +0200

    partial fix for docker-on-linux install scripts

--- deploy/docker-on-linux/install.sh
+++ deploy/docker-on-linux/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-INSTALL_PATH=$1
+ARCHIVE_PATH=$1
+INSTALL_PATH=$2
 
 echo "INSTALL_1 Install application to location '$INSTALL_PATH'"
-tar -zxf readable-stream-1.0.*.tgz -C $INSTALL_PATH
+tar -xf $ARCHIVE_PATH -C $INSTALL_PATH
--- deploy/docker-on-linux/setup.sh
+++ deploy/docker-on-linux/setup.sh
@@ -2,21 +2,24 @@
 
 INSTALL_PATH=$HOME/apps/easy-manage
 
+IMAGES_DIR=./images
+DIST_DIR=./dist
 LOGS_DIR=./logs
+
 DATETIME=$(date +"%Y-%m-%d_%H-%M-%S")
 LOGFILE=$LOGS_PATH/setup-$DATETIME.log
 
 echo "SETUP_1.1 Load backend docker image"
-docker load -i easy-manage-backend-latest.tar.gz
+docker load -i $IMAGES_DIR/backend-docker-image.tar.gz
 
 echo "SETUP_1.2 Load frontend docker image"
-docker load -i easy-manage-frontend-latest.tar.gz
+docker load -i $IMAGES_DIR/frontend-docker-image.tar.gz
 
 echo "SETUP_1.3 Load db docker image"
-docker load -i easy-manage-db-latest.tar.gz
+docker load -i $IMAGES_DIR/mongodb-docker-image.tar.gz
 
 echo "SETUP_2.1 Install the application"
-./install.sh $INSTALL_PATH 2>&1 > $LOGFILE
+./install.sh $DIST_DIR/easy-manage-docker-on-linux-bin.tar.gz $INSTALL_PATH 2>&1 > $LOGFILE
 
 echo "SETUP_3.1 Start the application"
 cd $INSTALL_PATH && ./start.sh 2>&1 > $LOGFILE
