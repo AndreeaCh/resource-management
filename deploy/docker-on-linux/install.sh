cefefe18282d66fbf5d5d4033f340c50ec84e74c
Marius Barta <marius.barta@gmail.com>
2019-03-01 17:21:24 +0200

    fix for docker-on-linux install scripts

--- deploy/docker-on-linux/install.sh
+++ deploy/docker-on-linux/install.sh
@@ -3,5 +3,8 @@
 ARCHIVE_PATH=$1
 INSTALL_PATH=$2
 
+echo "INSTALL_0 Create installation directories if necessary '$INSTALL_PATH'"
+mkdir -p $INSTALL_PATH
+
 echo "INSTALL_1 Install application to location '$INSTALL_PATH'"
-tar -xf $ARCHIVE_PATH -C $INSTALL_PATH
+tar -xf $ARCHIVE_PATH -C $INSTALL_PATH --strip 1
