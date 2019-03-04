c2af3dd9f31568a5f5efb357927f1369729a7fa3
Marius Barta <marius.barta@gmail.com>
2019-03-04 23:42:19 +0200

    docker on linux - introduced config file, removed hardcodings, added to PATH

--- deploy/docker-on-linux/install.sh
+++ deploy/docker-on-linux/install.sh
@@ -2,9 +2,25 @@
 
 ARCHIVE_PATH=$1
 INSTALL_PATH=$2
+SERVER_ADDRESS=${3:-localhost}
 
 echo "INSTALL_0 Create installation directories if necessary '$INSTALL_PATH'"
 mkdir -p $INSTALL_PATH
 
 echo "INSTALL_1 Install application to location '$INSTALL_PATH'"
 tar -xf $ARCHIVE_PATH -C $INSTALL_PATH --strip 1
+
+echo "INSTALL_2 Write config file"
+
+echo "INSTALL_PATH=$INSTALL_PATH" > ${HOME}/.easyman
+echo "SERVER_ADDRESS=$SERVER_ADDRESS" >> ${HOME}/.easyman
+echo "FRONTEND_RAM=1024000000" >> ${HOME}/.easyman
+echo "BACKEND_RAM=1024000000" >> ${HOME}/.easyman
+echo "DB_RAM=1024000000" >> ${HOME}/.easyman
+
+echo "INSTALL_3 Add bin to PATH"
+
+sed -i.bak '/export EASYMAN_HOME=/d' ${HOME}/.bashrc
+sed -i.bak '/PATH=$EASYMAN_HOME:$PATH/d' ${HOME}/.bashrc
+echo "export EASYMAN_HOME=$INSTALL_PATH" >> ${HOME}/.bashrc
+echo "PATH=\$EASYMAN_HOME:\$PATH" >> ${HOME}/.bashrc
--- deploy/docker-on-linux/setup.sh
+++ deploy/docker-on-linux/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+SERVER_ADDRESS=$1
 INSTALL_PATH=$HOME/apps/easy-manage
 
 IMAGES_DIR=./images
@@ -19,6 +20,6 @@ echo "SETUP_3/4 Load db docker image"
 docker load -i $IMAGES_DIR/mongodb-docker-image.tar.gz
 
 echo "SETUP_4/4 Install the application"
-./install.sh $DIST_DIR/easy-manage-docker-on-linux-bin.tar.gz $INSTALL_PATH 2>&1 > $LOGFILE
+./install.sh $DIST_DIR/easy-manage-docker-on-linux-bin.tar.gz $INSTALL_PATH $SERVER_ADDRESS 2>&1 > $LOGFILE
 
 echo "Application installed successfully. Run 'easyman' in a new shell for available options"
