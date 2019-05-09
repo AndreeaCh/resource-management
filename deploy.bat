0e628a57f2c0ea3c89c39c8b09338f3c2f643e3a
Marius Barta <marius.barta@gmail.com>
2019-05-10 00:28:48 +0300

    Partially fixed logging issues

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,4 @@ IF "%_JAR_PATH%"=="" SET _JAR_PATH=target/resource-management-0.0.1-SNAPSHOT.jar
 SET _CONFIG_PATH=%2
 IF "%_CONFIG_PATH%"=="" SET _CONFIG_PATH=D:\\logs\\application.properties
 
-java -Dspring.config.location=%_CONFIG_PATH% -jar %_JAR_PATH%
+java -Dspring.config.location=%_CONFIG_PATH% -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -jar %_JAR_PATH%
